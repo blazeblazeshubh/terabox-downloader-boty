a1b0a27f54962f8bebc00c824156d4fccbba0ab3
blazeblazeshubh <208866318+blazeblazeshubh@users.noreply.github.com>
2025-04-24 02:12:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,23 +1,23 @@
 API_ID = 123456  # api id
-API_HASH = "ABC-DEF1234ghIkl-zyx57W2v1u123ew11"  # api hash
+API_HASH = "c04ffceafc935c311084782a53f2c93e"  # api hash
 
-BOT_TOKEN = "123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11"  # bot token
+BOT_TOKEN = "7974398640:AAFJYxNbwGC9GZx7RfxIiVxxYDE3nHQGyWw"  # bot token
 
 
 # REDIS
-HOST = "localhost"  # redis host uri
-PORT = 6379  # redis port
-PASSWORD = ""  # redis password
+HOST = "redis-11536.c330.asia-south1-1.gce.redns.redis-cloud.com"  # redis host uri
+PORT = 11536  # redis port
+PASSWORD = "bN09NebEUFX2TgHSbcwF8yDGoGFL7QEa"  # redis password
 
-PRIVATE_CHAT_ID = -1001234567890  # CHAT WHERE YOU WANT TO STORE VIDEOS
+PRIVATE_CHAT_ID = -1002634106098  # CHAT WHERE YOU WANT TO STORE VIDEOS
 # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=;
-COOKIE = ""
-ADMINS = [1317173146]
+COOKIE = "PANWEB=1; csrfToken=zcB-8zYpUSuGJYARruP6cika; lang=en; TSID=UG8Ux11m0tQJhha2h6AYF6HoRuElxvMU; bid_n=18c0b12d393d1a88c44207; _ga=GA1.1.534448201.1700992834; _stripe_mid=63de2b36-0aa9-48d3-b049-d92a35378933041del; ndus-Y-Rose7teHuivLNA0f1oAiHAzBWCLRCIovVHKTb; browserid=cP5XWdCGKn9XXeBSULS9_aCyY3XzUgKTIo5xMGHMbeYH3WJwFiJvauWfTg8=; _ga_06ZNKL8C2E=GS1.1. 1707931403.23.1.1707931712.60.0.0; ndut_fmt=12D0AD9E2CF492FD104EBC622EF5D522561B603C27A46F87CE8864493BDC8353"
+ADMINS = [6774460505]
 
 
-BOT_USERNAME = "teraboxdown_bot"
+BOT_USERNAME = "@TeraboxCrushbot"
 
 # Force user to join this channel. (make sure you have promoted the bot on this chat.)
-FORCE_LINK = "@RoldexVerse"
+FORCE_LINK = "@huhhuhuhuhuuj"
 
 PUBLIC_EARN_API = ""  # https://publicearn.com/api
